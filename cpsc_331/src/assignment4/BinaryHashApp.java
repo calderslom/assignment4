b52823426613b3abbb207cf58d75cab98c1f74bc
C.Kalin <87056237+10162051@users.noreply.github.com>
2023-06-04 19:27:55 -0600

    Added HashTable and tried to work on BinaryHashApp

---
+++ cpsc_331/src/assignment4/BinaryHashApp.java
@@ -0,0 +1,75 @@
+package assignment4;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/** 
+ * This is the class that executes a multitude of search algorithms and outputs the timed results of those 
+ * searches to a text file in the working directory named a4_time_stamps.txt. The file is rewritten after every execution of the program.
+ * 
+ * Please note that the over documentation of code is to simplify the development process for the authors.
+ * 
+ * @author Calder Sloman
+ * @author Carlene Kalin
+ * 
+ */
+public class BinaryHashApp {
+	static final String OUTPUT_FILE = "a4_binary_hash_stamps.txt";
+	static final int MIN = 1000;
+	static final int MAX = 1000000;
+	
+	public static void main(String[] args) {
+		//ValueGenerator.generateSearchValues();											// Generates a file with all the values we will use in the search array (this ensures we use the same values for all tests)
+		int[] elements = ArrayGenerator.generateElementsArray();							
+		int[] searchArray;
+		long binaryStart, binaryEnd, binaryTotal, hashStart, hashEnd, hashTotal;
+		System.out.println("The results of the search expiriment are written to the " + OUTPUT_FILE + " in your working directory.\n");
+		
+		try {
+            // Creating objects for output
+			FileWriter fileWriter = new FileWriter(OUTPUT_FILE);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            System.out.println("The arrangement of data in the output file is as follows -");
+            System.out.println("\tArray Length:\tBinary Search:\tHash Search:");
+    			
+    			// Start of Hash Search and Sequential Search
+	    		
+	    		for (int n = MIN; n <= MAX; n+= 1000) {
+	    			searchArray = IntegerFileReader.returnArray(n);						// Creates an array based on values from the input file (this keeps arrays consistent across all searches and was recommended by Dr. Kawash)
+	        		// Add hash table population and search here
+					HashTable hashTable = new HashTable();
+	        		hashStart = System.nanoTime();
+	    			for (int element : elements) {
+						hashTable.search(element);
+	    			}
+	    			hashEnd = System.nanoTime();
+	    			hashTotal = hashEnd - hashStart;
+	    			
+	    			
+	    			QuickSort.quickSort(searchArray);									// Sorting the search array to prepare for Binary Search
+	        		binaryStart = System.nanoTime();									// START binary search for this array iteration
+	        		for (int element : elements) {
+	        			BinarySearch.searchArray(element, searchArray);					
+	        		}
+	        		binaryEnd = System.nanoTime();										// END binary search for this array iteration
+	        		binaryTotal = binaryEnd - binaryStart;
+	        		
+
+	    			
+	    			bufferedWriter.write(n + "\t" + binaryTotal + "\t" + hashTotal + "\n");
+	    			searchArray = null;
+	    		}
+    		
+            
+            
+            bufferedWriter.flush(); 
+			bufferedWriter.close();
+			fileWriter.close();
+			System.out.println("\nProgram Terminating");
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+	}	// End of Main
+	
+}	// End of Class
---
+++ cpsc_331/src/assignment4/HashTable.java
@@ -0,0 +1,53 @@
+package assignment4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+class HashTable {
+    private static final int TABLE_SIZE = 9973;     //Set the size of the table. 9973 was the req. from the assignment
+
+    private List<List<Integer>> table;
+
+    public HashTable() {
+        table = new ArrayList<>(TABLE_SIZE);
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            table.add(new ArrayList<>());           //Initialize each table with empty chains for each bucket
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public void insert(int key) {                   //Add keys to the hash table
+        int index = hash(key);
+        List<Integer> chain = table.get(index);     //Find the index of the bucket for each key we're adding
+        chain.add(key);                             //Add the key to the chain
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public boolean search(int key) {                 //Gets the chain and checks if the key's in it
+        int index = hash(key);
+        List<Integer> chain = table.get(index);      //Get the index of the hash bucket
+        return chain.contains(key);                  //Check if the key is in the chain with the index we got
+    }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void delete(int key) {                   //Delete keys from the hash table
+        int index = hash(key);
+        List<Integer> chain = table.get(index);     //Same as insert. Find the index.
+        chain.remove(Integer.valueOf(key));         //Make sure the key is treated like and int and not an index!
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private int hash(int x) {                       //How the hash values are calculated as specified in the assignment doc
+        x = ((x >>> 16) ^ x) * 0x45d9f3b;
+        x = ((x >>> 16) ^ x) * 0x45d9f3b;
+        x = (x >>> 16) ^ x;
+        return Math.abs(x) % TABLE_SIZE;
+    }
+}
